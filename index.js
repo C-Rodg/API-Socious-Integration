d443fcb696e1c879fce12db55f2b6171c99559b7
C-Rodg <rodgersbc@gmail.com>
2017-01-06 14:27:49 -0800

    Built functionality to make request and save to file

--- index.js
+++ index.js
@@ -2,8 +2,79 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 const CryptoJS = require('crypto-js');
-const request = require('request');
 const js2xmlparser = require('js2xmlparser');
+const Promise = require('bluebird');
+const axios = require('axios');
 
 const settings = require('./settings');
-console.log(settings.socious.eventId);
+const soap = require('./soapRequest');
+
+const js2xmlOptions = {
+    declaration : {
+        include : false
+    },
+    arrayMap : {
+        updates : "update"
+    }
+};
+
+function getSociousURL() {
+    const timeObject = new Date();
+    const timestamp = Math.round(timeObject.getTime() / 1000);
+    const signatureSecret = settings.socious.eventId + settings.socious.action + timestamp;
+    const signatureBytes = CryptoJS.HmacSHA256(signatureSecret, settings.socious.apiSecret);
+    const signatureText = signatureBytes.toString();
+
+    const url = `${settings.socious.baseURL}?action=${settings.socious.action}&eventid=${settings.socious.eventId}&timestamp=${timestamp}&apikey=${settings.socious.apiKey}&signature=${signatureText}`;
+    // TODO: ADD '&updates=1' after first request...
+
+    return url;
+}
+
+function makeSociousRequest() {
+    return new Promise((resolve, reject) => {
+        const sociousURL = getSociousURL();
+        axios.get(sociousURL)
+            .then((response) => {
+                console.log("Made Socious request");
+                resolve(response);
+            }).catch((err) => {
+                console.log(err);
+                reject(err);
+            });
+    });
+}
+
+function writeCSVtoFile(csvStream) {
+    return new Promise((resolve, reject) => {
+        const csvWriter = fs.createWriteStream('./attendees.csv');
+        csvWriter.write(csvStream.data);
+        csvWriter.end();
+        console.log("Wrote stream to attendees.csv");
+        resolve();
+    });
+}
+
+function readCSVFile() {
+    return new Promise((resolve, reject) => {
+        let registrantRawArray = [];
+        const readCSVStream = fs.createReadStream('attendees.csv').pipe(csv());
+        readCSVStream.on('data', (data) => {
+            registrantRawArray.push(data);
+        }).on('end', () => {
+            console.log(registrantRawArray.length);
+            resolve(registrantRawArray);
+        });
+    });
+}
+
+function startApplication() {
+    makeSociousRequest()
+        .then(writeCSVtoFile)
+        .then(readCSVFile)
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+startApplication();
---
+++ soapRequest.js
@@ -0,0 +1,14 @@
+function getSoapRequest(user, pass, guid, updates) {
+    return `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+             <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+             <soap:Header><AuthenticationSoapHeader xmlns="https://portal.validar.com/">
+                <Username>${user}</Username><Password>${pass}</Password>
+             </AuthenticationSoapHeader></soap:Header>
+             <soap:Body><PutRegistrationData xmlns="https://portal.validar.com/">
+             <eventGuid>${guid}</eventGuid><data><![CDATA[
+                 ${updates}
+            ]]></data></PutRegistrationData></soap:Body></soap:Envelope>   
+    `;
+}
+
+exports.getSoapRequest = getSoapRequest;
